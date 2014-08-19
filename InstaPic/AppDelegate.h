8f5dbad1b540027238e83e29060bbd2ee09da88e
SuperSickler <steven.sickler@yahoo.com>
2014-08-18 20:51:14 -0500

    Initial Commit

---
+++ InstaPic/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  InstaPic
+//
+//  Created by Steven Sickler on 8/18/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InstaPic/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  InstaPic
+//
+//  Created by Steven Sickler on 8/18/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
